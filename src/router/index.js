388574e10f8389bf2a5b8d3329761d20c0533b72
Katherine Huang <hello@katmh.com>
2021-01-10 20:14:32 -0500

    Dynamic routes hello world

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,40 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "home",
+    component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
+    path: "/about",
+    name: "about",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  {
+    path: "/404",
+    name: "404",
+    component: () => import("../views/PageNotFound.vue"),
+  },
+  {
+    path: "/:productPath",
+    name: "Product",
+    props: true,
+    component: () => import("../views/Product.vue"),
+  },
+];
 
 const router = new VueRouter({
-  routes
-})
+  mode: "history",
+  routes,
+});
 
-export default router
+export default router;
---
+++ src/views/Product.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    Product Displayed: {{ product }}
+  </div>
+</template>
+
+<script>
+const products = [
+  {
+    path: "example-product-1",
+    product: {name: "Product 1", price: 500}
+  },
+  {
+    path: "example-product-2",
+    product: {name: "Product 2", price: 1000}
+  },
+  {
+    path: "example-product-3",
+    product: {name: "Product 3", price: 1500}
+  }
+]
+export default {
+  props: ["productPath"],
+  data () {
+    return {
+      product: {}
+    }
+  },
+  created () {
+   // look up the product
+   const foundProduct = products.filter(product => {
+     return product.path === this.productPath
+   });
+    // if it exists, proceed
+    if (foundProduct.length > 0) {
+      this.product = foundProduct;
+    } else {
+      // otherwise, go to the 404 page
+      this.$router.push("/404");
+    }
+  }
+}
+</script>
