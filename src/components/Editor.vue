683f59d95d393bb160828199f77a2e53333a70c2
Katherine Huang <hello@katmh.com>
2021-01-12 16:37:38 -0500

    Collapsible -> summary, details; format header

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,5 +1,6 @@
 <template>
-  <Collapsible :url="url">
+  <details>
+    <EditorHeader :url="url" />
     <section id="editor">
       <div id="row_labels" class="row">
         <span class="label">Default</span>
@@ -27,17 +28,17 @@
         <button id="update_btn_text" class="btn">Update</button>
       </div>
     </section>
-  </Collapsible>
+  </details>
 </template>
 
 <script>
-import Collapsible from './Collapsible.vue';
+import EditorHeader from './EditorHeader.vue';
 
 export default {
   name: "Editor",
   props: ["words", "buttonText", "url"],
   components: {
-    Collapsible
+    EditorHeader
   },
   methods: {
     addSynonym(word, target) {
@@ -49,134 +50,141 @@ export default {
 </script>
 
 <style scoped>
-  #editor {
-    width: 100%;
-    background: #222;
+details {
+  width: 100%;
+  background: #222;
+  overflow: scroll;
+  padding: 0 1.3rem 1.3rem;
+  border-top-right-radius: 0.5rem;
+  border-top-left-radius: 0.5rem;
+}
+
+@media (min-width: 600px) {
+  details {
+    width: 95%;
+    position: absolute;
+    bottom: 0;
+    left: 2.5%;
+    max-height: 60vh;
     overflow: scroll;
-    padding: 0 1.3rem 1.3rem;
   }
+}
 
-  .label {
-    color: #ddd;
-    font-weight: bold;
-    margin: 0.5rem 0 0.25rem;
-  }
+.label {
+  color: #ddd;
+  font-weight: bold;
+  margin: 0.5rem 0 0.25rem;
+}
 
-
-  #header {
-    display: flex;
-    justify-content: space-between;
-    flex-wrap: wrap;
-  }
-  h2 {
-    color: #eee;
-    margin-bottom: 0.5rem;
-    text-transform: uppercase;
-    font-size: 1.25rem;
-  }
+h2 {
+  color: #eee;
+  margin-bottom: 0.5rem;
+  text-transform: uppercase;
+  font-size: 1.25rem;
+}
 
 
+.row {
+  display: grid;
+  margin-bottom: 1rem;
+}
+#row_labels {
+  display: none;
+}
+.row input, .field_row input {
+  margin: 0.3rem 0;
+  font-size: 1.15rem;
+  padding: 0.4rem 0.7rem;
+}
+.synonyms_container input {
+  font-size: 1rem;
+}
+
+
+.default_word_container {
+  display: flex;
+  align-items: flex-start;
+  margin-right: 1rem;
+}
+.delete_word {
+  background: transparent;
+  border: none;
+  color: #f66;
+  font-size: 1rem;
+  margin-right: 0.5rem;
+  margin-top: 0.75rem;
+  cursor: pointer;
+}
+.delete_word:hover {
+  color: #f33;
+}
+input {
+  border-bottom: 1px solid #656565;
+}
+input.default_word {
+  width: 100%;
+}
+
+
+.synonyms_container {
+  display: flex;
+  flex-wrap: wrap;
+}
+.synonym button {
+  padding: 0.4rem 0.5rem;
+  margin: 0.3rem 0.4rem 0.1rem 0;
+  border-radius: 0.25rem;
+  border: none;
+  cursor: pointer;
+  font-size: 1rem;
+  background: #ddd;
+  transition: 0.1s;
+}
+.synonym button:hover {
+  background: #efefef;
+}
+.synonym button:after {
+  content: " ✕";
+  color: #f33;
+  margin-left: 0.25rem;
+}
+
+
+#new_word {
+  display: block;
+  margin: 0.5rem 0 1rem;
+}
+
+
+.field_row {
+  display: block;
+  margin-top: 2rem;
+}
+.field_row input {
+  width: 100%;
+  margin: 0.5rem 0;
+}
+
+
+@media (min-width: 600px) {
   .row {
-    display: grid;
-    margin-bottom: 1rem;
+    grid-template-columns: 1fr 3fr;
   }
   #row_labels {
-    display: none;
+    display: grid;
   }
-  .row input, .field_row input {
-    margin: 0.3rem 0;
-    font-size: 1.15rem;
-    padding: 0.4rem 0.7rem;
-  }
-  .synonyms_container input {
-    font-size: 1rem;
-  }
-
-
-  .default_word_container {
-    display: flex;
-    align-items: flex-start;
-    margin-right: 1rem;
-  }
-  .delete_word {
-    background: transparent;
-    border: none;
-    color: #f66;
-    font-size: 1rem;
-    margin-right: 0.5rem;
-    margin-top: 0.75rem;
-    cursor: pointer;
-  }
-  .delete_word:hover {
-    color: #f33;
-  }
-  input {
-    border-bottom: 1px solid #656565;
-  }
-  input.default_word {
-    width: 100%;
-  }
-
-
-  .synonyms_container {
-    display: flex;
-    flex-wrap: wrap;
-  }
-  .synonym button {
-    padding: 0.4rem 0.5rem;
-    margin: 0.3rem 0.4rem 0.1rem 0;
-    border-radius: 0.25rem;
-    border: none;
-    cursor: pointer;
-    font-size: 1rem;
-    background: #ddd;
-    transition: 0.1s;
-  }
-  .synonym button:hover {
-    background: #efefef;
-  }
-  .synonym button:after {
-    content: " ✕";
-    color: #f33;
-    margin-left: 0.25rem;
-  }
-
-
   #new_word {
-    display: block;
-    margin: 0.5rem 0 1rem;
+    width: calc(25% - 1rem);
   }
-
-
-  .field_row {
-    display: block;
-    margin-top: 2rem;
+  .field_row .label {
+    margin-right: 0.25rem;
   }
   .field_row input {
-    width: 100%;
-    margin: 0.5rem 0;
+    width: 50%;
+    min-width: 30rem;
   }
-  
-
-  @media (min-width: 600px) {
-    .row {
-      grid-template-columns: 1fr 3fr;
-    }
-    #row_labels {
-      display: grid;
-    }
-    #new_word {
-      width: calc(25% - 1rem);
-    }
-    .field_row .label {
-      margin-right: 0.25rem;
-    }
-    .field_row input {
-      width: 50%;
-      min-width: 30rem;
-    }
-    #update_btn_text {
-      margin-left: 0.75rem;
-    }
+  #update_btn_text {
+    margin-left: 0.75rem;
   }
+}
 </style>
---
+++ src/components/EditorHeader.vue
@@ -0,0 +1,93 @@
+<template>
+  <summary>
+    <h2>Customize</h2>
+    <div id="url_container">
+      <label id="url_label" for="url">Shareable URL: </label>
+      <div id="url_and_btn">
+        <input id="url_input" name="url" type="text" v-bind:value="url" readonly />
+        <button id="url_btn" @click="copy">Copy</button>
+      </div>
+    </div>
+  </summary>
+</template>
+
+<script>
+export default {
+  name: 'EditorHeader',
+  props: ["url"],
+  methods: {
+    copy() {
+      const field = document.getElementById("url");
+      field.select();
+      field.setSelectionRange(0, 999999);
+      document.execCommand("copy");
+    }
+  },
+};
+</script>
+
+<style scoped>
+summary {
+  background: #222;
+  color: #eee;
+  padding: 1rem 0 0.5rem;
+  cursor: pointer;
+}
+
+h2 {
+  margin: 0;
+  font-size: 1.15rem;
+  display: inline-block;
+  vertical-align: middle;
+}
+
+#url_label {
+  color: #ddd;
+  font-weight: bold;
+  font-size: 1.15rem;
+  margin: 1rem 0 0.5rem;
+  display: inline-block;
+}
+
+#url_input {
+  width: 100%;
+  height: 1.5rem;
+  font-size: 0.95rem;
+  margin-right: 0.5rem;
+}
+
+#url_btn {
+  height: 1.5rem;
+}
+
+#url_and_btn {
+  display: flex;
+  margin-bottom: 1rem;
+}
+
+@media (min-width: 600px) {
+  h2 {
+    margin: 0 0 0 0.25rem;
+  }
+  #url_container {
+    float: right;
+  }
+  #url_label {
+    margin: 0;
+  }
+  #url_input {
+    width: 15rem;
+    margin: 0 0.5rem 0 0.25rem;
+  }
+  #url_and_btn {
+    display: inline-block;
+    margin: 0;
+  }
+}
+
+@media (min-width: 700px) {
+  #url_input {
+    width: 20rem;
+  }
+}
+</style>
