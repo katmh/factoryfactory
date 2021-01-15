ca755c7277217a32684bbcac0ced7e80dd5edebf
Katherine Huang <hello@katmh.com>
2021-01-15 01:09:04 -0500

    Maybe can't handle e

--- functions/makeConfig.js
+++ functions/makeConfig.js
@@ -1,11 +1,14 @@
 exports.handler = async function(event) {
-  const { words, buttonText } = event.body;
-  return fetch("https://jsonbox.io/box_23ae45336b05b999ce70", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ words, buttonText }),
-  })
-    .then((res) => res.json())
-    .then((data) => ({ statusCode: 200, data }))
-    .catch((e) => ({ statusCode: 503, e }));
+  try {
+    return fetch("https://jsonbox.io/box_23ae45336b05b999ce70", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(event.body),
+    })
+      .then((res) => res.json())
+      .then((data) => ({ statusCode: 200, data: data }))
+      .catch((e) => ({ statusCode: 503, error: e.toString() }));
+  } catch (e) {
+    return { statusCode: 501, error: e.toString() };
+  }
 };
