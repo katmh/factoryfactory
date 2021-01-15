890a7249114f021432c24aadd9687ff04f5e228a
Katherine Huang <hello@katmh.com>
2021-01-15 00:29:26 -0500

    Try fetch function

--- functions/makeConfig.js
+++ functions/makeConfig.js
@@ -1,16 +1,16 @@
-const { default: axios } = require("axios");
-
 exports.handler = async function(event) {
   try {
-    const { words, buttonText } = JSON.parse(event.body);
-    return axios
-      .post("https://jsonbox.io/box_23ae45336b05b999ce70", {
-        words,
-        buttonText,
-      })
-      .then((res) => ({ statusCode: 200, body: JSON.stringify({ res }) }))
-      .catch((e) => ({ statusCode: 503, body: e.toString() }));
+    return fetch("https://jsonbox.io/box_23ae45336b05b999ce70", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: event.body,
+    })
+      .then((res) => res.json())
+      .then((data) => ({ statusCode: 200, data }))
+      .catch((e) => ({ statusCode: 503, e }));
   } catch (e) {
-    return { statusCode: 501, body: e.toString() };
+    return { statusCode: 501, e };
   }
 };
