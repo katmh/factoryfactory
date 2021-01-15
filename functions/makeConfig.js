fcd3ce1a9d25247793eae648379457d0b93e5efe
Katherine Huang <hello@katmh.com>
2021-01-15 00:49:38 -0500

    Headers obj

--- functions/makeConfig.js
+++ functions/makeConfig.js
@@ -1,10 +1,9 @@
 exports.handler = async function(event) {
-  const { words, buttonText } = JSON.parse(event.body);
   try {
     return fetch("https://jsonbox.io/box_23ae45336b05b999ce70", {
       method: "POST",
-      headers: "Content-Type: application/json",
-      body: { words, buttonText },
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(event.body),
     })
       .then((res) => res.json())
       .then((data) => ({ statusCode: 200, data }))
