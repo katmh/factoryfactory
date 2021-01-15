9c50dab808c118ed64e6e7c3f763252ead65939b
Katherine Huang <hello@katmh.com>
2021-01-15 00:53:47 -0500

    Destructure again?

--- functions/makeConfig.js
+++ functions/makeConfig.js
@@ -1,9 +1,10 @@
 exports.handler = async function(event) {
+  const { words, buttonText } = event.body;
   try {
     return fetch("https://jsonbox.io/box_23ae45336b05b999ce70", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(event.body),
+      body: JSON.stringify({ words, buttonText }),
     })
       .then((res) => res.json())
       .then((data) => ({ statusCode: 200, data }))
