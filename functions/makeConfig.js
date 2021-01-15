f096ee91632d303d9aaa256c6fbb3d02365ed6cf
Katherine Huang <hello@katmh.com>
2021-01-15 01:01:53 -0500

    Remove try catch

--- functions/makeConfig.js
+++ functions/makeConfig.js
@@ -1,15 +1,11 @@
 exports.handler = async function(event) {
   const { words, buttonText } = event.body;
-  try {
-    return fetch("https://jsonbox.io/box_23ae45336b05b999ce70", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ words, buttonText }),
-    })
-      .then((res) => res.json())
-      .then((data) => ({ statusCode: 200, data }))
-      .catch((e) => ({ statusCode: 503, e }));
-  } catch (e) {
-    return { statusCode: 501, e };
-  }
+  return fetch("https://jsonbox.io/box_23ae45336b05b999ce70", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ words, buttonText }),
+  })
+    .then((res) => res.json())
+    .then((data) => ({ statusCode: 200, data }))
+    .catch((e) => ({ statusCode: 503, e }));
 };
